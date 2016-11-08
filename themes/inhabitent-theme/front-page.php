e23443faaa53a5160ed616c2145d7de2f68f5649
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-08 02:55:17 -0800

    flexed front-page pictures

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -13,7 +13,9 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
     <section class="home-hero">
 
-<div class="logo-header"></div>
+<div class="logo-header">
+
+</div>
     </section>
 
       <?php while ( have_posts() ) : the_post(); ?>
@@ -21,21 +23,26 @@ get_header(); ?>
         <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
       <?php endwhile; // End of the loop. ?>
-<p>this is a test</p>
+
+<h2>Latest Adventures</h2>
 <section class="latest-adventures">
-  <h2>Latest Adventures</h2>
+
 
     <div class="canoe-girl">
         <span class="adventure-photos"><a href="#">Getting Back to Nature in a Canoe</a></span>
     </div>
+    <div class="bundle1">
     <div class="beach-bonfire">
         <span class="adventure-photos"><a href="#">A Night with Friends at the Beach</a></span>
     </div>
+    <div class="bundle2">
     <div class="mountain-hikers">
         <span class="adventure-photos"><a href="#">Taking in the View at Big Mountain</a></span>
     </div>
     <div class="night-sky">
         <span class="adventure-photos"><a href="#">Star-Gazing at the Night Sky</a></span>
+    </div>
+    </div>
     </div>
 
 
