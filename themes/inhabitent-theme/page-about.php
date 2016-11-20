e6c3d22cad001905e93eda1b4ba160df629e5c38
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-20 14:00:42 -0800

    added our team and out story using CFS->get

--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -15,17 +15,22 @@ get_header(); ?>
 <div class="about-content">
 <h2>our story</h2>
 
-<p>
- Inhabitent Camping Supply Co. has been Vancouver baked-good icon for more than two whole months! Customers often comment on the bustle of activity they see in store…that’s where the magic happens every day.
+<?php
 
-We want to bridge the gap between the comfort of city life and the lovely Instagram-worthiness of the great outdoors that surround us. We sell gear that’s fun and functional. So much fun, in fact, that you’ll want to pitch a tent inside your one-bedroom apartment so you can use it everyday.
-</p>
+ echo $cfs->get('our_story');
+
+?>
+
 <h2>our team</h2>
-<p>
-  Inhabitent Camping Supply Co.’s staff is made up of an amazing team of inspired retail associates. We really know our stuff when it comes to travel hammocks and campfire cooking gadgets. From a provincial park campground to the back-country, our staff knows what you need to outfit your outdoor outing.<br>
 
-Our shop in nestled away in a lovely little corner of Vancouver. Pop in, say hi, and try out our tents!
-</p>
+<?php
+
+ echo $cfs->get('our_team');
+
+?>
+
 </div>
+
+
 
 <?php get_footer(); ?>
